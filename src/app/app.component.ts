8b97c6db576f272671d42b22c1ff1c34b9196430
soumyaRauth <shoummorauth@gmail.com>
2021-02-05 03:45:50 +0600

    angular reddit app first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-reddit';
+ addArticle(title:HTMLInputElement,link:HTMLInputElement){
+   console.log(`title is ${title.value} and Link is ${link.value}`)
+ }
 }
